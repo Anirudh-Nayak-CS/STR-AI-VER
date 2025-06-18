6fd9da434d74fabbbfedc8129291b5fbee70c72c
Anirudh Nayak <anirudhnayak1508@gmail.com>
2025-06-18 19:14:28 +0530

    Hero section done

--- react/frontend/src/App.jsx
+++ react/frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import {Navbars} from './Navbars'
+import { HeroSection } from './HeroSection'
 function App() {
   
 
   return (
     <>
         <Router>
+         
            <Navbars/>
+           <HeroSection/>
           </Router>
     </>
   )
